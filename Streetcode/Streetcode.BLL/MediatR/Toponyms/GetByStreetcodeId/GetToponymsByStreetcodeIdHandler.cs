bd57fe7a23ac85969a6f8ad37275e5117e24f9db
Ілля Цап <its.experimental.version.0@outlook.com>
2026-05-11 20:21:09 +0300

    fix/45: move DistinctBy to the db side

--- Streetcode/Streetcode.BLL/MediatR/Toponyms/GetByStreetcodeId/GetToponymsByStreetcodeIdHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Toponyms/GetByStreetcodeId/GetToponymsByStreetcodeIdHandler.cs
@@ -1,13 +1,13 @@
 using AutoMapper;
 using FluentResults;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using Streetcode.BLL.DTO.Toponyms;
+using Microsoft.EntityFrameworkCore;
 using Streetcode.BLL.Interfaces.Logging;
-using Streetcode.DAL.Entities.Toponyms;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 
 #pragma warning disable SA1111 //Closing parenthesis should be on line of last parameter
+#pragma warning disable SA1513 //Closing brace should be followed by blank line
 
 namespace Streetcode.BLL.MediatR.Toponyms.GetByStreetcodeId;
 
@@ -26,20 +26,18 @@ public class GetToponymsByStreetcodeIdHandler : IRequestHandler<GetToponymsByStr
 
     public async Task<Result<IEnumerable<ToponymDTO>>> Handle(GetToponymsByStreetcodeIdQuery request, CancellationToken cancellationToken)
     {
-        IEnumerable<Toponym> toponyms = await _repositoryWrapper.ToponymRepository.GetAllAsync(
-            predicate: sc => sc.Streetcodes.Any(s => s.Id == request.StreetcodeId),
-            include: scl => scl.Include(sc => sc.Coordinate)
-        );
+        IEnumerable<ToponymDTO> toponyms = _repositoryWrapper.ToponymRepository.FindAll(
+            sc => sc.Streetcodes.Any(s => s.Id == request.StreetcodeId)
+        ).AsNoTracking().Include(sc => sc.Coordinate).DistinctBy(
+            x => x.StreetName
+        ).AsEnumerable().Select(_mapper.Map<ToponymDTO>);
+
         if(toponyms.Any() is false)
         {
             string errorMsg = $"Cannot find any toponym by the streetcode id: {request.StreetcodeId}";
             _logger.LogError(request, errorMsg);
             return Result.Fail(new Error(errorMsg));
         }
-
-        var toponymDtos = toponyms
-    .DistinctBy(x => x.StreetName)
-    .Select(_mapper.Map<ToponymDTO>);
-        return Result.Ok(toponym_dto);
+        return Result.Ok(toponyms);
     }
 }
