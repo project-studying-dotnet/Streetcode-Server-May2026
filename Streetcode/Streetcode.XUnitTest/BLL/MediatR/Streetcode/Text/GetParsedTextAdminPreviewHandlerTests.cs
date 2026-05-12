5402dd3234c2eda6185f8c7e39fddf48bd52edb4
Nikita <nikita.s.pankin@gmail.com>
2026-05-13 01:01:08 +0300

    Add test Handle_ReturnsError_WhenServiceReturnsNull

---
+++ Streetcode/Streetcode.XUnitTest/BLL/MediatR/Streetcode/Text/GetParsedTextAdminPreviewHandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Moq;
+using Streetcode.BLL.Interfaces.Text;
+using Streetcode.BLL.MediatR.Streetcode.Text.GetParsed;
+using Xunit;
+
+namespace Streetcode.XUnitTest.BLL.MediatR.Streetcode.Text;
+
+public class GetParsedTextAdminPreviewHandlerTests
+{
+    private readonly Mock<ITextService> textServiceMock;
+    private readonly GetParsedTextAdminPreviewHandler handler;
+
+    public GetParsedTextAdminPreviewHandlerTests()
+    {
+        this.textServiceMock = new Mock<ITextService>();
+        this.handler = new GetParsedTextAdminPreviewHandler(this.textServiceMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ReturnsError_WhenServiceReturnsNull()
+    {
+        // Arrange
+        var command = new GetParsedTextForAdminPreviewCommand(textToParse: "some text");
+
+        this.textServiceMock
+            .Setup(s => s.AddTermsTag(command.textToParse))
+            .ReturnsAsync((string?)null);
+
+        // Act
+        var result = await this.handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.IsFailed.Should().BeTrue();
+        result.Errors.Should().ContainSingle(e => e.Message == "text was not parsed successfully");
+    }
+}
