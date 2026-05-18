77185629988fdd1bf55de39ddaa7109e20c71e9b
Ілля Цап <its.experimental.version.0@outlook.com>
2026-05-18 09:10:23 +0300

    fix/43: forward CancellationToken from handler to method ToListAsync()

--- Streetcode/Streetcode.BLL/MediatR/Toponyms/GetByStreetcodeId/GetToponymsByStreetcodeIdHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Toponyms/GetByStreetcodeId/GetToponymsByStreetcodeIdHandler.cs
@@ -4,14 +4,10 @@ using FluentResults;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Streetcode.BLL.DTO.Toponyms;
-using Microsoft.EntityFrameworkCore;
 using Streetcode.BLL.Interfaces.Logging;
 using Streetcode.DAL.Repositories.Interfaces.Base;
-using AutoMapper.QueryableExtensions;
-using Streetcode.DAL.Entities.Toponyms;
 
 #pragma warning disable SA1111 //Closing parenthesis should be on line of last parameter
-#pragma warning disable SA1513 //Closing brace should be followed by blank line
 
 namespace Streetcode.BLL.MediatR.Toponyms.GetByStreetcodeId;
 
@@ -30,16 +26,13 @@ public class GetToponymsByStreetcodeIdHandler : IRequestHandler<GetToponymsByStr
 
     public async Task<Result<IEnumerable<ToponymDTO>>> Handle(GetToponymsByStreetcodeIdQuery request, CancellationToken cancellationToken)
     {
-        var toponyms = await _repositoryWrapper.ToponymRepository.FindAll(
+        List<ToponymDTO> toponyms = await _repositoryWrapper.ToponymRepository.FindAll(
             sc => sc.Streetcodes.Any(s => s.Id == request.StreetcodeId)
-        )
-        .DistinctBy(t => t.StreetName)
-        .ProjectTo<ToponymDTO>(_mapper.ConfigurationProvider)
-        .ToListAsync();
-
-        if (toponyms.Count == 0)
+        ).DistinctBy(t => t.StreetName).ProjectTo<ToponymDTO>(_mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+        
+        if(toponyms.Count == 0)
         {
-            var errorMsg = $"Cannot find any toponym by the streetcode id: {request.StreetcodeId}";
+            string errorMsg = $"Cannot find any toponym by the streetcode id: {request.StreetcodeId}";
             _logger.LogError(request, errorMsg);
             return Result.Fail(new Error(errorMsg));
         }
