7b47af8639f3af0bffde389659ed3e20f34de343
Ілля Цап <its.experimental.version.0@outlook.com>
2026-05-19 12:50:40 +0300

    fix/81: rewriting handler and fix unconvertible query problem

--- Streetcode/Streetcode.BLL/MediatR/Toponyms/GetByStreetcodeId/GetToponymsByStreetcodeIdHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Toponyms/GetByStreetcodeId/GetToponymsByStreetcodeIdHandler.cs
@@ -1,41 +1,37 @@
-using AutoMapper;
-using AutoMapper.QueryableExtensions;
-using FluentResults;
 using MediatR;
+using AutoMapper;
+using FluentResults;
 using Microsoft.EntityFrameworkCore;
 using Streetcode.BLL.DTO.Toponyms;
 using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.DAL.Entities.Toponyms;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 
 #pragma warning disable SA1111 //Closing parenthesis should be on line of last parameter
 
 namespace Streetcode.BLL.MediatR.Toponyms.GetByStreetcodeId;
 
-public class GetToponymsByStreetcodeIdHandler : IRequestHandler<GetToponymsByStreetcodeIdQuery, Result<IEnumerable<ToponymDTO>>>
+public sealed class GetToponymsByStreetcodeIdHandler(
+    IRepositoryWrapper repositoryWrapper,
+    IMapper mapper,
+    ILoggerService logger
+) : IRequestHandler<GetToponymsByStreetcodeIdQuery, Result<IEnumerable<ToponymDTO>>>
 {
-    private readonly IMapper _mapper;
-    private readonly IRepositoryWrapper _repositoryWrapper;
-    private readonly ILoggerService _logger;
-
-    public GetToponymsByStreetcodeIdHandler(IRepositoryWrapper repositoryWrapper, IMapper mapper, ILoggerService logger)
-    {
-        _repositoryWrapper = repositoryWrapper;
-        _mapper = mapper;
-        _logger = logger;
-    }
-
     public async Task<Result<IEnumerable<ToponymDTO>>> Handle(GetToponymsByStreetcodeIdQuery request, CancellationToken cancellationToken)
     {
-        List<ToponymDTO> toponyms = await _repositoryWrapper.ToponymRepository.FindAll(
+        List<Toponym> toponyms = await repositoryWrapper.ToponymRepository.FindAll(
             sc => sc.Streetcodes.Any(s => s.Id == request.StreetcodeId)
-        ).DistinctBy(t => t.StreetName).ProjectTo<ToponymDTO>(_mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+        ).GroupBy(t => t.StreetName).Select(
+            g => g.First()
+        ).ToListAsync(cancellationToken);
+
         if(toponyms.Count == 0)
         {
             string errorMsg = $"Cannot find any toponym by the streetcode id: {request.StreetcodeId}";
-            _logger.LogError(request, errorMsg);
+            logger.LogError(request, errorMsg);
             return Result.Fail(new Error(errorMsg));
         }
 
-        return Result.Ok(toponyms.AsEnumerable());
+        return Result.Ok(mapper.Map<IEnumerable<ToponymDTO>>(toponyms));
     }
 }
