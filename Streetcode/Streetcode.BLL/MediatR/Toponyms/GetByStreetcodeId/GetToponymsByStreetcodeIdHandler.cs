d9b6d8c4c8e699b4858e80f9d033c8abb61d83f1
Ілля Цап <its.experimental.version.0@outlook.com>
2026-05-19 14:59:28 +0300

    style: fix code format warnings

--- Streetcode/Streetcode.BLL/MediatR/Toponyms/GetByStreetcodeId/GetToponymsByStreetcodeIdHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Toponyms/GetByStreetcodeId/GetToponymsByStreetcodeIdHandler.cs
@@ -7,23 +7,20 @@ using Streetcode.BLL.Interfaces.Logging;
 using Streetcode.DAL.Entities.Toponyms;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 
-#pragma warning disable SA1111 //Closing parenthesis should be on line of last parameter
-
 namespace Streetcode.BLL.MediatR.Toponyms.GetByStreetcodeId;
 
 public sealed class GetToponymsByStreetcodeIdHandler(
     IRepositoryWrapper repositoryWrapper,
     IMapper mapper,
-    ILoggerService logger
-) : IRequestHandler<GetToponymsByStreetcodeIdQuery, Result<IEnumerable<ToponymDTO>>>
+    ILoggerService logger)
+    : IRequestHandler<GetToponymsByStreetcodeIdQuery, Result<IEnumerable<ToponymDTO>>>
 {
     public async Task<Result<IEnumerable<ToponymDTO>>> Handle(GetToponymsByStreetcodeIdQuery request, CancellationToken cancellationToken)
     {
         List<Toponym> toponyms = await repositoryWrapper.ToponymRepository.FindAll(
-            sc => sc.Streetcodes.Any(s => s.Id == request.StreetcodeId)
-        ).GroupBy(t => t.StreetName).Select(
-            g => g.First()
-        ).ToListAsync(cancellationToken);
+            sc => sc.Streetcodes.Any(s => s.Id == request.StreetcodeId)).GroupBy(t => t.StreetName)
+            .Select(g => g.First())
+            .ToListAsync(cancellationToken);
 
         if(toponyms.Count == 0)
         {
