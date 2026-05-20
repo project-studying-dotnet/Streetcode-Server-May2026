6b4ac8339a62fd606977e0f92b177449a72d098f
Інна Шпонька <inneta3@gmail.com>
2026-05-20 10:29:10 +0300

    refactor: fix CS8618 warning in TextCreateDTO,TextUpdateDTO  by initializing strings

--- Streetcode/Streetcode.BLL/DTO/Streetcode/TextContent/Text/TextCreateDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Streetcode/TextContent/Text/TextCreateDTO.cs
@@ -2,8 +2,8 @@ namespace Streetcode.BLL.DTO.Streetcode.TextContent.Text
 {
   public class TextCreateDTO
   {
-    public string Title { get; set; }
-    public string TextContent { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string TextContent { get; set; } = string.Empty;
     public int StreetcodeId { get; set; }
     public string? AdditionalText { get; set; }
   }
--- Streetcode/Streetcode.BLL/DTO/Streetcode/TextContent/Text/TextUpdateDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Streetcode/TextContent/Text/TextUpdateDTO.cs
@@ -7,8 +7,8 @@ namespace Streetcode.BLL.DTO.Streetcode.TextContent.Text
     public class TextUpdateDTO
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string TextContent { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string TextContent { get; set; } = string.Empty;
         public int StreetcodeId { get; set; }
         public string? AdditionalText { get; set; }
     }
