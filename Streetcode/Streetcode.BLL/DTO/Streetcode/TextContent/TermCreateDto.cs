27fa05f781d10cee5a6d68129524bc0ef030c37a
andrxpie <andrxpie@gmail.com>
2026-05-21 17:00:18 +0300

    Add Dto for Create Term.

---
+++ Streetcode/Streetcode.BLL/DTO/Streetcode/TextContent/TermCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Streetcode.BLL.DTO.Streetcode.TextContent
+{
+    public class TermCreateDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
