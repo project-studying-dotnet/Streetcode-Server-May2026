8b325fde6f447697ab3c05c00cfc19b02bd3d9b6
Інна Шпонька <inneta3@gmail.com>
2026-05-21 10:27:51 +0300

    dalete commit

--- Streetcode/Streetcode.BLL/DTO/Media/Video/VideoCreateDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Media/Video/VideoCreateDTO.cs
@@ -6,8 +6,8 @@ namespace Streetcode.BLL.DTO.Media.Video
 {
     public class VideoCreateDto
     {
-        ////public string Title { get; set; } = string.Empty;
-        ////public string? Description { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
         public string? Url { get; set; }
         public int StreetcodeId { get; set; }
     }
