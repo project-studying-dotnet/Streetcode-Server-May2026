a08b1c8728a00d469b027b6a9ea7a45482af5bc6
andrxpie <andrxpie@gmail.com>
2026-05-26 00:01:14 +0300

    Add Term/Delete Endpoint.

--- Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/TermController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/TermController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Streetcode.BLL.DTO.Streetcode.TextContent;
 using Streetcode.BLL.MediatR.Streetcode.Term.Create;
+using Streetcode.BLL.MediatR.Streetcode.Term.Delete;
 using Streetcode.BLL.MediatR.Streetcode.Term.GetAll;
 using Streetcode.BLL.MediatR.Streetcode.Term.GetById;
 
@@ -24,5 +25,11 @@ public class TermController : BaseApiController
     public async Task<IActionResult> Create([FromBody] CreateTermDto term)
     {
         return HandleResult(await Mediator.Send(new CreateTermCommand(term)));
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete([FromRoute] int id)
+    {
+        return HandleResult(await Mediator.Send(new DeleteTermCommand(id)));
     }
 }
