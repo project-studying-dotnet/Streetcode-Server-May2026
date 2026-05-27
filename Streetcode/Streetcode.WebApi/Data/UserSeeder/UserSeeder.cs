7d1806a4caa12024d260eb08a7c5913f332817e3
Vlad <Vlad@VLADPC>
2026-05-27 17:40:57 +0300

    fix: sonar

--- Streetcode/Streetcode.WebApi/Data/UserSeeder/UserSeeder.cs
+++ Streetcode/Streetcode.WebApi/Data/UserSeeder/UserSeeder.cs
@@ -12,38 +12,6 @@ namespace Streetcode.WebApi.InitialData.UserSeeder
     {
         public static async Task FillSeedAsync(StreetcodeDbContext dbContext, IConfiguration configuration)
         {
-            // var adminEmail = configuration["AdminSettings:Email"];
-            // var adminPassword = configuration["AdminSettings:Password"];
-
-            // var identityPasswordHasher = new PasswordHasher<User>();
-
-            // var adminUser = await dbContext.Users.FirstOrDefaultAsync(u => u.UserName == "admin");
-
-            // if (adminUser == null)
-            // {
-            //    var newAdmin = new User
-            //    {
-            //        Email = "admin@admin.com",
-            //        NormalizedEmail = "ADMIN@ADMIN.COM",
-            //        UserName = AdminLiteral,
-            //        NormalizedUserName = "ADMIN",
-            //        Name = AdminLiteral,
-            //        Surname = AdminLiteral,
-            //        Role = UserRole.MainAdministrator,
-            //        EmailConfirmed = true,
-            //    };
-
-            // newAdmin.PasswordHash = identityPasswordHasher.HashPassword(newAdmin, AdminLiteral);
-
-            // await dbContext.Users.AddAsync(newAdmin);
-            //    await dbContext.SaveChangesAsync();
-            // }
-            // else if (string.IsNullOrEmpty(adminUser.PasswordHash) || adminUser.PasswordHash.Length < 20)
-            // {
-            //    adminUser.PasswordHash = identityPasswordHasher.HashPassword(adminUser, AdminLiteral);
-
-            // await dbContext.SaveChangesAsync();
-            // }
         }
     }
 }
