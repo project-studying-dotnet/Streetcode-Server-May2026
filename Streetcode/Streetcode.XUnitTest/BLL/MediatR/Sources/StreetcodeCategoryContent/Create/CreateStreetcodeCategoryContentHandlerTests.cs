a9e81179e91fd476ccd22742a47aabdb5e22ba76
harasymm <markiangarasim7328@gmail.com>
2026-05-31 18:03:52 +0300

    Add unit tests directories for CRUD Sources Content handlers

---
+++ Streetcode/Streetcode.XUnitTest/BLL/MediatR/Sources/StreetcodeCategoryContent/Create/CreateStreetcodeCategoryContentHandlerTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Streetcode.XUnitTest.BLL.MediatR.Sources.StreetcodeCategoryContent.Create
+{
+    internal class CreateStreetcodeCategoryContentHandlerTests
+    {
+    }
+}
