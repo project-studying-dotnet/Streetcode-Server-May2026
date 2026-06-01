0cbe72ea062db768e59c16b8e7bacc01697733da
Vasia1101 <vasilij.gajda@gmail.com>
2026-06-01 18:17:16 +0300

    Update the logic of DeleteRelatedTermCommand and handler (#160)
    * Update the logic of DeleteRelatedTermCommand and handler
    
    * added missed test

--- Streetcode/Streetcode.XUnitTest/BLL/Validators/Streetcode/RelatedTerm/DeleteRelatedTermCommandValidatorTests.cs
+++ Streetcode/Streetcode.XUnitTest/BLL/Validators/Streetcode/RelatedTerm/DeleteRelatedTermCommandValidatorTests.cs
@@ -20,27 +20,39 @@ namespace Streetcode.XUnitTest.Validators.Streetcode.RelatedTerm.Delete
         [InlineData("   ")]
         public void Should_Have_Error_When_Word_Is_Empty(string? invalidWord)
         {
-            var command = new DeleteRelatedTermCommand(invalidWord!);
+            var command = new DeleteRelatedTermCommand(invalidWord!, 1);
 
             var result = _validator.TestValidate(command);
 
-            result.ShouldHaveValidationErrorFor(x => x.word);
+            result.ShouldHaveValidationErrorFor(x => x.Word);
         }
 
         [Fact]
         public void Should_Have_Error_When_Word_Exceeds_Maximum_Length()
         {
-            var command = new DeleteRelatedTermCommand(new string('a', 256));
+            var command = new DeleteRelatedTermCommand(new string('a', 256), 1);
 
             var result = _validator.TestValidate(command);
 
-            result.ShouldHaveValidationErrorFor(x => x.word);
+            result.ShouldHaveValidationErrorFor(x => x.Word);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void Should_Have_Error_When_TermId_Is_Not_Positive(int invalidTermId)
+        {
+            var command = new DeleteRelatedTermCommand("Валідний термін", invalidTermId);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.TermId);
         }
 
         [Fact]
         public void Should_Not_Have_Errors_When_Command_Is_Valid()
         {
-            var command = new DeleteRelatedTermCommand("Валідний термін");
+            var command = new DeleteRelatedTermCommand("Валідний термін", 1);
 
             var result = _validator.TestValidate(command);
 
@@ -50,7 +62,7 @@ namespace Streetcode.XUnitTest.Validators.Streetcode.RelatedTerm.Delete
         [Fact]
         public void Should_Not_Have_Errors_When_Word_Is_Exactly_At_Maximum_Length()
         {
-            var command = new DeleteRelatedTermCommand(new string('a', 255));
+            var command = new DeleteRelatedTermCommand(new string('a', 255), 1);
 
             var result = _validator.TestValidate(command);
 
