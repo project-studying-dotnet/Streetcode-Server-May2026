2dec27a9db060a3e0f57ce04d0e379fe6d8dc9c1
Vasia1101 <vasilij.gajda@gmail.com>
2026-06-04 17:48:37 +0300

    feat(email-service): create EmailService project

---
+++ Streetcode/Streetcode.EmailService/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/health", () => Results.Ok("EmailService is running"))
+    .WithName("HealthCheck");
+
+app.Run();
