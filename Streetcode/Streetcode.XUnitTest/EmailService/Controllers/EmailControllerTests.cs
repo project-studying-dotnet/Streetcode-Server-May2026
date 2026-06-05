f79d4a3c021a48b40d0a31e33134f3330f458699
Vasia1101 <vasilij.gajda@gmail.com>
2026-06-05 14:37:24 +0300

    feat(email-service): add hangfire support to email service (#207)

--- Streetcode/Streetcode.XUnitTest/EmailService/Controllers/EmailControllerTests.cs
+++ Streetcode/Streetcode.XUnitTest/EmailService/Controllers/EmailControllerTests.cs
@@ -1,57 +1,50 @@
 ﻿using FluentAssertions;
+using Hangfire;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
 using Streetcode.EmailService.Controllers;
-using Streetcode.EmailService.Interfaces;
-using Streetcode.EmailService.Models;
-using Streetcode.XUnitTest.EmailService.Constants;
 using Streetcode.EmailService.Models.Requests;
+using Streetcode.XUnitTest.EmailService.Constants;
 
-namespace Streetcode.EmailService.XUnitTest.Controllers;
+namespace Streetcode.XUnitTest.EmailService.Controllers;
 
 public class EmailControllerTests
 {
-    private readonly Mock<IEmailService> _emailServiceMock = new();
+    private readonly Mock<IBackgroundJobClient> _backgroundJobClientMock = new();
+    private readonly EmailController _controller;
+
+    public EmailControllerTests()
+    {
+        _controller = new EmailController(_backgroundJobClientMock.Object);
+    }
+
+    [Fact]
+    public void Send_ShouldReturnAccepted_WhenEmailIsQueued()
+    {
+        // Arrange
+        var request = CreateRequest();
+
+        _backgroundJobClientMock
+            .Setup(client => client.Create(
+                It.IsAny<Hangfire.Common.Job>(),
+                It.IsAny<Hangfire.States.IState>()))
+            .Returns("job-id");
+
+        // Act
+        var result = _controller.Send(request);
+
+        // Assert
+        result.Should().BeOfType<AcceptedResult>();
+
+        _backgroundJobClientMock.Verify(
+            client => client.Create(
+                It.IsAny<Hangfire.Common.Job>(),
+                It.IsAny<Hangfire.States.IState>()),
+            Times.Once);
+    }
+
     private static SendEmailRequest CreateRequest() => new()
-    {
-        To = [EmailTestConstants.TestEmail],
-        From = EmailTestConstants.FromEmail,
-        Subject = EmailTestConstants.Subject,
-        Content = EmailTestConstants.Content,
-    };
-
-    [Fact]
-    public async Task Send_ShouldReturnOk_WhenEmailSentSuccessfully()
-    {
-        var controller = new EmailController(_emailServiceMock.Object);
-        var request = CreateRequest();
-
-        _emailServiceMock
-            .Setup(x => x.SendEmailAsync(It.IsAny<Message>()))
-            .ReturnsAsync(true);
-
-        var result = await controller.Send(request);
-
-        result.Should().BeOfType<OkObjectResult>();
-    }
-
-    [Fact]
-    public async Task Send_ShouldReturnBadRequest_WhenEmailSendingFails()
-    {
-        var controller = new EmailController(_emailServiceMock.Object);
-        var request = CreateRequest();
-
-        _emailServiceMock
-            .Setup(x => x.SendEmailAsync(It.IsAny<Message>()))
-            .ReturnsAsync(false);
-
-        var result = await controller.Send(request);
-
-        result.Should().BeOfType<BadRequestObjectResult>();
-    }
-
-    private static Message CreateMessage() => new()
     {
         To = [EmailTestConstants.TestEmail],
         From = EmailTestConstants.FromEmail,
