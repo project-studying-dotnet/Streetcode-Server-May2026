fe23196edd994ac73ba223339e411efe86be16ac
Vasia1101 <vasilij.gajda@gmail.com>
2026-06-05 10:30:54 +0300

    add more test cases

---
+++ Streetcode/Streetcode.XUnitTest/EmailService/Services/SmtpClientWrapperTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Streetcode.EmailService.Services;
+using Xunit;
+
+namespace Streetcode.XUnitTest.EmailService.Services;
+
+public class SmtpClientWrapperTests
+{
+    [Fact]
+    public async Task DisposeAsync_ShouldCompleteSuccessfully()
+    {
+        // Arrange
+        var wrapper = new SmtpClientWrapper();
+
+        // Act
+        var act = async () => await wrapper.DisposeAsync();
+
+        // Assert
+        await act.Should().NotThrowAsync();
+    }
+
+    [Fact]
+    public async void IsConnected_ShouldBeFalse_WhenClientIsNotConnected()
+    {
+        // Arrange
+        var wrapper = new SmtpClientWrapper();
+        await wrapper.DisposeAsync();
+
+        // Act
+        var result = wrapper.IsConnected;
+
+        // Assert
+        result.Should().BeFalse();
+    }
+}
