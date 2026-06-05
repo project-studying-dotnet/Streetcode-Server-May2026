bf91b4e1a802809ffb60992e7b6af10dbb001d83
Vasia1101 <vasilij.gajda@gmail.com>
2026-06-05 11:56:04 +0300

    feat(email-service): add email microservice with MailKit (#204)
    * feat(email-service): add email microservice with MailKit
    
    * add unit test cases
    
    * add email service test
    
    * fix sonar issue
    
    * add more test cases
    
    * fix test sonar warning

--- Streetcode/Streetcode.EmailService/Program.cs
+++ Streetcode/Streetcode.EmailService/Program.cs
@@ -1,17 +1,33 @@
+using Streetcode.EmailService.Interfaces;
+using Streetcode.EmailService.Models;
+using Streetcode.EmailService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+builder.Services.Configure<EmailConfiguration>(
+    builder.Configuration.GetSection("EmailConfiguration"));
+
+builder.Services.AddScoped<ISmtpClientFactory, SmtpClientFactory>();
+builder.Services.AddScoped<IEmailService, EmailService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.MapGet("/health", () => Results.Ok("EmailService is running"))
-   .WithName("HealthCheck");
+    .WithName("HealthCheck");
+
+app.MapControllers();
 
 await app.RunAsync();
