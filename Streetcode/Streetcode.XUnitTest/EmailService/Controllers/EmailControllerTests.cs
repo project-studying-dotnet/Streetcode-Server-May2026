7d1a79b89bf78ed95d9df307854c058c6be08ae3
Vasia1101 <vasilij.gajda@gmail.com>
2026-06-05 09:41:59 +0300

    add unit test cases

---
+++ Streetcode/Streetcode.XUnitTest/EmailService/Controllers/EmailControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+using Streetcode.EmailService.Controllers;
+using Streetcode.EmailService.Interfaces;
+using Streetcode.EmailService.Models;
+using Streetcode.XUnitTest.EmailService.Constants;
+
+namespace Streetcode.EmailService.XUnitTest.Controllers;
+
+public class EmailControllerTests
+{
+    private readonly Mock<IEmailService> _emailServiceMock = new();
+
+    [Fact]
+    public async Task Send_ShouldReturnOk_WhenEmailSentSuccessfully()
+    {
+        var controller = new EmailController(_emailServiceMock.Object);
+        var message = CreateMessage();
+
+        _emailServiceMock
+            .Setup(x => x.SendEmailAsync(message))
+            .ReturnsAsync(true);
+
+        var result = await controller.Send(message);
+
+        result.Should().BeOfType<OkObjectResult>();
+    }
+
+    [Fact]
+    public async Task Send_ShouldReturnBadRequest_WhenEmailSendingFails()
+    {
+        var controller = new EmailController(_emailServiceMock.Object);
+        var message = CreateMessage();
+
+        _emailServiceMock
+            .Setup(x => x.SendEmailAsync(message))
+            .ReturnsAsync(false);
+
+        var result = await controller.Send(message);
+
+        result.Should().BeOfType<BadRequestObjectResult>();
+    }
+
+    private static Message CreateMessage() => new()
+    {
+        To = [EmailTestConstants.TestEmail],
+        From = EmailTestConstants.FromEmail,
+        Subject = EmailTestConstants.Subject,
+        Content = EmailTestConstants.Content,
+    };
+}
---
+++ Streetcode/Streetcode.XUnitTest/EmailService/Models/EmailConfigurationTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Xunit;
+using Streetcode.EmailService.Models;
+using Streetcode.XUnitTest.EmailService.Constants;
+
+namespace Streetcode.EmailService.XUnitTest.Models;
+
+public class EmailConfigurationTests
+{
+    [Fact]
+    public void Should_Set_Properties_Correctly()
+    {
+        var config = new EmailConfiguration
+        {
+            From = EmailTestConstants.FromEmail,
+            SmtpServer = EmailTestConstants.SmtpServer,
+            Port = EmailTestConstants.Port,
+            UserName = EmailTestConstants.UserName,
+            Password = EmailTestConstants.Password,
+        };
+
+        config.From.Should().Be(EmailTestConstants.FromEmail);
+        config.SmtpServer.Should().Be(EmailTestConstants.SmtpServer);
+        config.Port.Should().Be(EmailTestConstants.Port);
+        config.UserName.Should().Be(EmailTestConstants.UserName);
+        config.Password.Should().Be(EmailTestConstants.Password);
+    }
+}
