7e3a5dc2e346e4d137c63c5ef04eea7a22d1b6ba
AnnSnitko <annsnitko.com.com@gmail.com>
2026-06-08 21:04:48 +0200

    fix

--- Streetcode/Streetcode.WebApi/Controllers/Comments/CommentController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Comments/CommentController.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Streetcode.BLL.DTO.Comments;
 using Streetcode.BLL.MediatR.Comments.Create;
+using Streetcode.DAL.Enums;
+using Streetcode.WebApi.Attributes;
 
 namespace Streetcode.WebApi.Controllers.Comments;
 
 public class CommentController : BaseApiController
 {
     [HttpPost]
+    [AuthorizeRoles(UserRole.Administrator)]
     public async Task<IActionResult> Create([FromBody] CreateCommentDto request, CancellationToken cancellationToken = default)
     {
         return HandleResult(await Mediator.Send(new CreateCommentCommand(request), cancellationToken));
