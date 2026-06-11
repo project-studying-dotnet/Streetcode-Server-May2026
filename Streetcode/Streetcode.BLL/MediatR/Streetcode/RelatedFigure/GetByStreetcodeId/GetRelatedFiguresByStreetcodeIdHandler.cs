b546735343f992b7f7982a98ea5f4386fb87fdb6
Ілля Цап <its.experimental.version.0@outlook.com>
2026-06-11 19:15:49 +0300

    task/56: fix sonar issues

--- Streetcode/Streetcode.BLL/MediatR/Streetcode/RelatedFigure/GetByStreetcodeId/GetRelatedFiguresByStreetcodeIdHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/RelatedFigure/GetByStreetcodeId/GetRelatedFiguresByStreetcodeIdHandler.cs
@@ -70,7 +70,7 @@ public class GetRelatedFiguresByStreetcodeIdHandler : IRequestHandler<GetRelated
         }
         catch (ArgumentNullException)
         {
-            return null;
+            return Enumerable.Empty<int>().AsQueryable();
         }
     }
 }
--- Streetcode/Streetcode.DAL/Repositories/Interfaces/Streetcode/IRelatedFigureRepository.cs
+++ Streetcode/Streetcode.DAL/Repositories/Interfaces/Streetcode/IRelatedFigureRepository.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Linq;
-using System.Linq.Expressions;
-using Streetcode.DAL.Entities.Streetcode;
+﻿using Streetcode.DAL.Entities.Streetcode;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 
-namespace Streetcode.DAL.Repositories.Interfaces.Streetcode
-{
-    public interface IRelatedFigureRepository : IRepositoryBase<RelatedFigure>
-    {
-        IQueryable<RelatedFigure> FindAll(Expression<Func<RelatedFigure, bool>> expression);
-    }
-}
+namespace Streetcode.DAL.Repositories.Interfaces.Streetcode;
+
+public interface IRelatedFigureRepository : IRepositoryBase<RelatedFigure>;
